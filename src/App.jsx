9f2dbf16042d45b01b63cd0bb52aca90f6fd8299
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-05-24 21:10:53 +0530

    created components Navbar,NewsBoard,NewsItems then created navbar and heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import Navbar from './components/Navbar'
+import NewsBoard from './components/NewsBoard'
 function App() {
 
   return (
     <>
-       
+       <Navbar/>
+       <NewsBoard/>
     </>
   )
 }
